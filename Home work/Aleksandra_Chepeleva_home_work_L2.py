47cdce9bdfaf5f6ce8151b505be1c526c7fa4809
Sasha <achepeleva.fs@gmail.com>
2025-04-18 21:50:33 +0300

    Add file with home work L2

---
+++ Home work/Aleksandra_Chepeleva_home_work_L2.py
@@ -0,0 +1,56 @@
+# # Home work 2.1
+# # Найти наибольшее из чисел.
+
+# num1 = int(input('Введи число num1 -> '))
+# num2 = int(input('Введи число num2 -> '))
+# num3 = int(input('Введи число num3 -> '))
+# num4 = int(input('Введи число num4 -> '))
+
+# if num1 > num2:
+#     largest_from_num1_num2 = num1
+# elif num1 == num2:
+#     largest_from_num1_num2 = num1
+#     print('The num1 is equal to the num2')
+# else:
+#     largest_from_num1_num2 = num2
+
+# if num3 > num4:
+#     largest_from_num3_num4 = num3
+# elif num3 == num4:
+#     largest_from_num3_num4 = num3
+#     print('The num3 is equal to the num4')
+# else:
+#     largest_from_num3_num4 = num4
+
+# if largest_from_num1_num2 > largest_from_num3_num4:
+#     largest_fnumber_of_all = largest_from_num1_num2
+# elif largest_from_num1_num2 == largest_from_num3_num4:
+#     largest_fnumber_of_all = largest_from_num1_num2
+#     print('The largest_from_num1_num2 is equal to the largest_from_num3_num4')
+# else:
+#     largest_fnumber_of_all = largest_from_num3_num4
+
+# print('The largest number of all is -', largest_fnumber_of_all)
+
+
+# # Home work 2.6
+# # Calculator. Operations: + - / *.
+
+# math_operation = input('Введите "+ - / *" or "exit":')
+
+# while math_operation != "exit":
+#     num1 = int(input('Введите первое число ->'))
+#     num2 = int(input('Введите второе число ->'))
+
+#     if math_operation == '+':
+#         print('num1 + num2 =', num1 + num2 )
+#     elif math_operation == '-':
+#         print('num1 - num2 =', num1 - num2 )
+#     elif math_operation == '/':
+#         print('num1 - num2 =', num1 / num2 )
+#     elif math_operation == '*':
+#         print('num1 - num2 =', num1 * num2 )
+#     else:
+#         print('Не верная операция!')
+#     math_operation = input('Введите "+ - / *" or "exit":')
+
