9e4be056fdc2ffbdf221bf1b3665bf6ac3ec51ac
Sasha <achepeleva.fs@gmail.com>
2025-04-27 12:41:10 +0300

    Some changes in to do list and new file shoot dice easy

--- Home work/L_6/Aleksandra_Chepeleva_shoot_dice_easy.py
+++ Home work/L_6/Aleksandra_Chepeleva_shoot_dice_easy.py
@@ -1,1 +1,17 @@
 
+# import random as r
+
+# secret = r.randint(1,10)
+
+# my_number = int(input("Give me your number 1-10:"))
+
+# print(my_number == secret)
+# print("secret = ", secret)
+
+
+# secret = r.randint(1,5)
+
+# my_number = int(input("Give me your number 1-5:"))
+
+# print(my_number == secret)
+# print("secret = ", secret)
--- Home work/L_6/Aleksandra_chepeleva_to_do_list.py
+++ Home work/L_6/Aleksandra_chepeleva_to_do_list.py
@@ -1,62 +1,61 @@
-def create_task():
-    """Функция для создания задания."""
-    global task_auto_id
-    t_name = input("Дай имя задачи:")
-    t_status = False
-    task_list[task_auto_id] = {"task_name":t_name,"status":False}
-    task_auto_id += 1
-    return True
+# # To do list
+
+# def create_task():
+#     """Функция для создания задания."""
+#     global task_auto_id
+#     t_name = input("Дай имя задачи:")
+#     t_status = False
+#     task_list[task_auto_id] = {"task_name":t_name,"status":False}
+#     task_auto_id += 1
+#     return True
 
 
-def show_tasks():
-    """Функция для отображения всех заданий"""
-    print("show tasks")
-    for task_id, desc in task_list.items():
-        print("______________")
-        print(f"Task id: {task_id}")
-        for key, value in desc.items():
-            print("\t",key, value)
-        print("______________")
+# def show_tasks():
+#     """Функция для отображения всех заданий"""
+#     print("show tasks")
+
+#     if not task_list: # Проверка если словарь пустой.
+#         print("Заданий в списке пока нет, введи новое.")
+#     else:
+#         for task_id, desc in task_list.items():
+#             print("______________")
+#             print(f"Task id: {task_id}")
+#             for key, value in desc.items():
+#                 print("\t",key, value)
+#             print("______________")
 
 
-def change_status():
-    """Функция для изменения сатуса задания"""
-    task_id = int(input("Give me programm number:"))
-    if task_id not in task_list:
-        return False
-    inside = task_list.get(task_id)
-    inside["status"] = True
-    task_list.update({task_id:inside})
+# def change_status():
+#     """Функция для изменения сатуса задания"""
+#     task_id = int(input("Give me programm number:"))
+#     if task_id not in task_list:
+#         return False
+#     inside = task_list.get(task_id)
+#     inside["status"] = True
+#     task_list.update({task_id:inside})
 
 
-def main():
-    msg = """1 - new tasks
-    2 - show tasks
-    3 - change task status
-    quit - exit from programm
-    """
-    oper = input(msg)
+# def main():
+#     msg = """1 - new tasks
+#     2 - show tasks
+#     3 - change task status
+#     quit - exit from programm
+#     """
+#     oper = input(msg)
 
-    while oper != "quit":
-        match oper:
-            case "1":
-                create_task()
-            case "2":
-                show_tasks()
-            case "3":
-                change_status()
-            case _:
-                print("Sorry, ne ponial")
-        oper = input(msg)
-
-
-task_list = {}
-task_auto_id = 1
-main()
+#     while oper != "quit":
+#         match oper:
+#             case "1":
+#                 create_task()
+#             case "2":
+#                 show_tasks()
+#             case "3":
+#                 change_status()
+#             case _:
+#                 print("Sorry, ne ponial")
+#         oper = input(msg)
 
 
-
-# добавить 
-#документацию
-#улучшить вывод
-#структурировать код
+# task_list = {}
+# task_auto_id = 1
+# main()
